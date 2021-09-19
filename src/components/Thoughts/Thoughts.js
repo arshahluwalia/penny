d90be50fbd44f4068d7f6d1781b655d310bfa7e2
Nadeen <nelshaw@uwo.ca>
2021-09-18 21:42:39 -0400

    Organize thought page

--- src/components/Thoughts/Thoughts.js
+++ src/components/Thoughts/Thoughts.js
@@ -4,69 +4,79 @@ import "./Thoughts.css";
 function Thoughts () {
 
     const [thoughts, setThoughts] = useState([]);
-    const [currThought, setCurrThought] = useState([]);
-    const [thoughtIndex, setThoughtIndex] = useState(-1);
 
     useEffect(() => {
         // Pull thoughts from DB
         setThoughts([
             {
-                thought: 'random thought'
+                thought: 'people who back into parking lots like what do you have to prove',
+                saves: 428
             },
             {
-                thought: 'random thought 2'
+                thought: 'random thought 2',
+                saves: 428
             },
             {
-                thought: 'short thought'
+                thought: 'sometimes i sleep all day and still be sleepy',
+                saves: 428
             },
             {
-                thought: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies accumsan dui quis rutrum. Duis aliquam elit eget consequat volutpat. Fusce dolor tellus, fermentum in efficitur id, ultricies nec felis. Phasellus feugiat commodo rutrum. Sed tellus ligula, tristique at lorem.'
-            },{
-                thought: 'random thought'
+                thought: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies accumsan dui quis rutrum. Duis aliquam elit eget consequat volutpat. Fusce dolor tellus, fermentum in efficitur id, ultricies nec felis. Phasellus feugiat commodo rutrum. Sed tellus ligula, tristique at lorem.',
+                saves: 428
             },
             {
-                thought: 'random thought again'
+                thought: 'pineapple on pizza slander will not be tolerated in this house',
+                saves: 428
+            },
+            {
+                thought: 'oct-dec is the best time of the year',
+                saves: 428
+            },
+            {
+                thought: 'people who back into parking lots like what do you have to prove',
+                saves: 428
+            },
+            {
+                thought: 'random thought 2',
+                saves: 428
+            },
+            {
+                thought: 'sometimes i sleep all day and still be sleepy',
+                saves: 428
+            },
+            {
+                thought: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies accumsan dui quis rutrum. Duis aliquam elit eget consequat volutpat. Fusce dolor tellus, fermentum in efficitur id, ultricies nec felis. Phasellus feugiat commodo rutrum. Sed tellus ligula, tristique at lorem.',
+                saves: 428
+            },
+            {
+                thought: 'pineapple on pizza slander will not be tolerated in this house',
+                saves: 428
+            },
+            {
+                thought: 'oct-dec is the best time of the year',
+                saves: 428
             }
         ]);
-        setCurrThought({thought: ''});
     }, []);
 
-    useEffect(() => {
-        if(thoughts.length > 0) {
-            setCurrThought(thoughts[0]);
-        }
-    }, [thoughts]);
-
-    useEffect(() => {
-        setThoughtIndex(thoughtIndex + 1);
-    }, [currThought]);
-
-    function onNext() {
-        if(thoughtIndex < thoughts.length) {
-            setCurrThought(thoughts[thoughtIndex]);
-        } 
-        else {
-            alert("Oops! Looks like you've read all thoughts. Check back in when we get more.");
-        }
-    };
-
-    function onWithdraw() {
+    function onWithdraw(obj) {
         console.log("withdraw");
-        onNext();
+        console.log(obj);
     }
 
     return (
         <div className="thoughts">
             <div className="container">
-                <div className="thought">
-                    <div className="thought_box">
-                        <p>{currThought.thought}</p>
-                    </div>
-                </div>
-                <button className="withdraw" onClick={onWithdraw}>withdraw</button>
+                {thoughts.map((thought, i) => {
+                    return (<div key={i} className="thought">
+                        <p>{thought.thought}</p>
+                        <div className="saves">
+                            <button className="withdraw" onClick={onWithdraw}>Withdraw</button>
+                            <p>{thought.saves}</p>
+                        </div>
+                    </div>)
+                })}
             </div>
-
-            <button onClick={onNext}>next</button>
         </div>
     );
 };

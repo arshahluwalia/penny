62970f78e55ba82ddb40f4a39e71f95f57bafeab
Nadeen <nelshaw@uwo.ca>
2021-09-19 08:36:23 -0400

    Add firebase auth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Thoughts from './components/Thoughts/Thoughts';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Reset from './components/Reset/Reset';
 
 class App extends React.Component {
 
@@ -14,6 +17,9 @@ class App extends React.Component {
         <Router>
           <Header />
           <Switch>
+            <Route path="/login" exact>
+              <Login />
+            </Route>
             <Route path="/" exact>
               <Home />
             </Route>
@@ -22,6 +28,12 @@ class App extends React.Component {
             </Route>
             <Route path="/profile" exact>
               <Profile />
+            </Route>
+            <Route path="/register" exact>
+              <Register />
+            </Route>
+            <Route path="/reset" exact>
+              <Reset />
             </Route>
           </Switch>
         </Router>
---
+++ src/firebase.js
@@ -0,0 +1,89 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCGuFsT-l5SCM3Q6UGwlzpMNJgvXUlGnk",
+    authDomain: "penny-tech.firebaseapp.com",
+    projectId: "penny-tech",
+    storageBucket: "penny-tech.appspot.com",
+    messagingSenderId: "870088123689",
+    appId: "1:870088123689:web:18e1b878215af45f8a0338",
+    measurementId: "G-7ZQNW1EPE6"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+    try {
+        const res = await auth.signInWithPopup(googleProvider);
+        const user = res.user;
+        const query = await db
+            .collection("users")
+            .where("uid", "==", user.uid)
+            .get();
+        if (query.docs.length === 0) {
+            await db.collection("users").add({
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email,
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const signInWithEmailAndPassword = async (email, password) => {
+    try {
+        await auth.signInWithEmailAndPassword(email, password);
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+        const res = await auth.createUserWithEmailAndPassword(email, password);
+        const user = res.user;
+        await db.collection("users").add({
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const sendPasswordResetEmail = async (email) => {
+    try {
+        await auth.sendPasswordResetEmail(email);
+        alert("Password reset link sent!");
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const logout = () => {
+    auth.signOut();
+};
+
+export {
+    auth,
+    db,
+    signInWithGoogle,
+    signInWithEmailAndPassword,
+    registerWithEmailAndPassword,
+    sendPasswordResetEmail,
+    logout,
+};
